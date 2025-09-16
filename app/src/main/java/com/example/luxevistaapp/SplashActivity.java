b51682ed8ceb8be799b4d0ecd2654f38525f16c8
Charitha Wickramarathna <56013746+Charitha615@users.noreply.github.com>
2025-09-17 00:29:41 +0530

    updates

--- app/src/main/java/com/example/luxevistaapp/SplashActivity.java
+++ app/src/main/java/com/example/luxevistaapp/SplashActivity.java
@@ -1,11 +1,13 @@
 package com.example.luxevistaapp;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+@SuppressLint("CustomSplashScreen")
 public class SplashActivity extends AppCompatActivity {
 
     private static final int SPLASH_DELAY = 3000; // 3 seconds
